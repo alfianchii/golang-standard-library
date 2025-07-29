e54f0198adc2bac6b7f1382d1b4f090d3ed09131
alfianchii <pianaee379@gmail.com>
2025-07-29 21:01:03 +0700

    14-feat: encoding packages w/ csv read and write

--- 14-encoding/main.go
+++ 14-encoding/main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 )
 
 func main() {
@@ -21,4 +25,32 @@ func main() {
 	fmt.Println([]byte(name))
 	fmt.Println(string(name))
 	fmt.Println("================================================================================================================")
+
+	var csvString string = "alfian,taka\n" +
+		"galih,gacor\n" +
+		"moe,poi"
+	var reader *csv.Reader = csv.NewReader(strings.NewReader(csvString))
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Println(record)
+	}
+	fmt.Println("----------------")
+
+	var writer *csv.Writer = csv.NewWriter(os.Stdout)
+	_ = writer.Write([]string{"alfian", "taka"})
+	_ = writer.Write([]string{"galih", "gacor"})
+	_ = writer.Write([]string{"moe", "poi"})
+	writer.Flush()
+
+	_ = writer.WriteAll([][]string{
+		{"alfian", "taka"},
+		{"galih", "gacor"},
+		{"moe", "poi"},
+	})
+	writer.Flush()
 }
