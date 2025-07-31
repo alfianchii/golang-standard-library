b02f129d2c60255d77a3ca1c79ef3e0266b42795
alfianchii <pianaee379@gmail.com>
2025-07-31 20:25:02 +0700

    16-feat: path package w/ filepath

--- 16-path/main.go
+++ 16-path/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"path"
+	"path/filepath"
 )
 
 func main() {
@@ -10,4 +11,13 @@ func main() {
 	fmt.Println(path.Base("hello/world.go"))
 	fmt.Println(path.Ext("hello/world.go"))
 	fmt.Println(path.Join("hello", "world", "main.go"))
+	fmt.Println("================================================================================================================")
+
+	// filepath detects your OS
+	fmt.Println(filepath.Dir("hello/world.go"))
+	fmt.Println(filepath.Base("hello/world.go"))
+	fmt.Println(filepath.Ext("hello/world.go"))
+	fmt.Println(filepath.IsAbs("hello/world.go"))
+	fmt.Println(filepath.IsLocal("hello/world.go"))
+	fmt.Println(filepath.Join("hello", "world", "main.go"))
 }
